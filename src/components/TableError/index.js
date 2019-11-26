623d7dfab525354674a38e897f37817be8c1991f
Felipe Cooper <felipecardoso323@hotmail.com>
2019-11-26 00:28:46 -0300

    ADD: Archieved

--- src/components/TableError/index.js
+++ src/components/TableError/index.js
@@ -48,6 +48,20 @@ export default function TableError(data, functionClicked, setPage) {
       window.location.reload();
     }
   }
+  const archieveError = () => {
+    if (window.confirm('Deseja arquivar mesmo?')) {
+      options.map(del => {
+        let url = '/v1/error/aggregates/archived?' +
+          "environment=" + del.environment +
+          "&title=" + del.title +
+          "&origin=" + del.origin +
+          "&level=" + del.level;
+        api.patch(url)
+      })
+      alert("Erros arquivados")
+      window.location.reload();
+    }
+  }
   const pagination = {
     defaultCurrent: 1,
     total: (data.data.totalPages*10),
@@ -57,6 +71,7 @@ export default function TableError(data, functionClicked, setPage) {
     <div>
       <div style={{ marginBottom: 16, marginTop: 16 }}>
         <Button text="Deletar" onClick={ev => { deleteError() }} />
+        <Button text="Arquivar" onClick={ev => { archieveError() }} />
       </div>
       <Table pagination={ pagination } columns={columns}
         rowSelection={rowSelection}
